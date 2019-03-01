4b89e4432cf6a10d579f52c50e3040240c64c5c8
hornmo <moritz.horn@gmail.com>
2019-03-01 11:11:58 +0100

    moved for testing

--- server/gokbg3/grails-app/services/com/k_int/ESSearchService.groovy
+++ server/gokbg3/grails-app/services/com/k_int/ESSearchService.groovy
@@ -145,7 +145,8 @@ class ESSearchService{
         if ( params[mapping.key] instanceof String[] ) {
           log.debug("mapping is an arraylist: ${mapping} ${mapping.key} ${params[mapping.key]}")
           if(sw.toString()) sw.write(" AND ");
-          def plist = params[mapping.key]
+
+          def plist = params.list(mapping.key)
 
           plist.eachWithIndex { p, idx ->
             if (p) {
